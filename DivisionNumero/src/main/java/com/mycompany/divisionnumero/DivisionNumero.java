c4401be0000f8e2937ee1000d8a5009a3d52edcb
Andrès Patarroyo <A@gmail.com>
2024-08-01 19:44:14 -0500

    Version 1.0

---
+++ DivisionNumero/src/main/java/com/mycompany/divisionnumero/DivisionNumero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.divisionnumero;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class DivisionNumero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
